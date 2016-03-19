faa8725df61bbce94dae520b613c3d037209f493
somkiat <somkiat.p@gmail.com>
2016-03-19 14:57:41 +0700

    Add comment

--- lab07.hs
+++ lab07.hs
@@ -7,5 +7,6 @@ map' f (x:xs) = f x : map' f xs
 doubleAll xs = map' (*2) xs
 powerAll xs = map' (^2) xs
 
+-- แต่ไม่ค่อยนิยมเท่าไร เนื่องจากทำให้อ่านยาก
 doubleAll'  = map' (*2)
 powerAll' = map'(^2)
